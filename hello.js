135eb748113e7f16eaae23e2714c46a409d34d7b
lakhwinder555 <lakhwindersinghwins@gmail.com>
2024-08-30 15:54:59 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const { createServer } = require('http');
 
 const hostname = '127.0.0.1';
-const port = 8000;
+const port = 7000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
