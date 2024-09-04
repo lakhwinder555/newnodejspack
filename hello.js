54affd9e4192033da6885f5f1766bd1c6063300d
lakhwinder555 <lakhwindersinghwins@gmail.com>
2024-09-04 15:41:46 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,13 +1,11 @@
-const { createServer } = require('http');
-
-const hostname = 'localhost';
-const port = 2001;
+const hostname = '0.0.0.0';  // Bind to all interfaces
+const port = process.env.PORT || 2001;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Welcome to Lakhwinder Singh');
-}); 
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
